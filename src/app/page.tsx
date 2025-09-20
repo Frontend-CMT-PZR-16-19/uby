89ad8acb160582c0f66ff3cae379a9bef7e84a38
bedirhan-1 <bedirhan9828@gmail.com>
2025-09-20 16:50:58 +0300

    LTS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section className="relative h-[75vh]">
-      {/* Background image */}
       <Image
         src="/background-image.png"
         alt="Üçüncü binyıl hero image"
@@ -11,11 +10,7 @@ export default function Home() {
         priority
         className="object-cover object-center z-0"
       />
-
-      {/* Gradient overlay */}
       <div className="pointer-events-none absolute inset-0 z-[1] bg-gradient-to-r from-black via-black/50 to-transparent" />
-
-      {/* Content */}
       <div className="relative z-10 container mx-auto px-6 py-24">
         <div className="max-w-[50%]">
           <h1 className="text-8xl text-white font-semibold tracking-tight">
