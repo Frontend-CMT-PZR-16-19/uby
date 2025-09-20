00594165f525e8d69e62710c4a93134b20096b21
bedirhan-1 <bedirhan9828@gmail.com>
2025-09-20 17:09:08 +0300

    cidden bu son hal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
-import Image from "next/image";
+import { Hero } from "@/components/hero";
 
 export default function Home() {
   return (
-    <section className="relative h-[75vh]">
-      <Image
-        src="/background-image.png"
-        alt="Üçüncü binyıl hero image"
-        fill
-        priority
-        className="object-cover object-center z-0"
-      />
-      <div className="pointer-events-none absolute inset-0 z-[1] bg-gradient-to-r from-black via-black/50 to-transparent" />
-      <div className="relative z-10 container mx-auto px-6 py-24">
-        <div className="max-w-[50%]">
-          <h1 className="text-8xl text-white font-semibold tracking-tight">
-            <label className="bg-primary p-2 leading-32">Üçüncü Binyıl</label> ile geleceğini inşa et
-          </h1>
-        </div>
-      </div>
-    </section>
+    <>
+      <Hero />
+      {/* About */}
+      {/* Contact */}
+    </>
   );
 }
+
